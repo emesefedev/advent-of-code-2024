ccc66e297b06d658047d2e289b8e0fb1ac3b91a2
emesefedev <emesefedev@gmail.com>
2024-12-03 19:24:58 +0100

    feat: day2A solved

--- day02/day2.js
+++ day02/day2.js
@@ -1,10 +1,28 @@
 import { readFile } from "../utilities/file-manager.js"
+import { compareArrays, distancesBetweenElements } from "../utilities/array-functions.js";
 
 export async function day2A() {
 
     const reports = await day2Input()
+    const safeReports = []
 
-    
+    reports.forEach((report) => {
+        const sorted = report.toSorted();
+        
+        const levelDifferences = distancesBetweenElements(report)
+        
+        const isAscending = levelDifferences.every(element => element < 0)
+        const isDescending = levelDifferences.every(element => element > 0)
+        
+        const differencesInLimits = levelDifferences.every(element =>
+            isBetweenLimits(Math.abs(element)))
+
+        const isSafe = (isAscending || isDescending) && differencesInLimits
+        safeReports.push(isSafe)
+    })
+
+    const totalSafeReports = safeReports.filter(isSafe => isSafe).length
+    console.log(`There are ${totalSafeReports} safe reports`)
 }
 
 export async function day2B() {
@@ -14,7 +32,7 @@ export async function day2B() {
 
 
 async function day2Input() {
-    const lines = await readFile({filePath: "test-input.txt"})
+    const lines = await readFile({filePath: "day02/day2-input.txt"})
 
     const reports = []
 
@@ -22,7 +40,10 @@ async function day2Input() {
         const report = line.trim().split(/\s+/).map(Number)
         reports.push(report)
     })
-
-    console.log(reports)
+    
     return reports
+}
+
+function isBetweenLimits(element) {
+    return element >= 1 && element <= 3
 }
--- utilities/array-functions.js
+++ utilities/array-functions.js
@@ -1,3 +1,15 @@
 export function frequency(array, element) {
     return array.filter(e => e === element).length
 }
+
+export function compareArrays(a, b) {
+    if (a.length !== b.length) return false
+    
+    return a.every((element, idx) => element === b[idx]);
+}
+
+export function distancesBetweenElements(array){
+    return array
+      .slice(1)
+      .map((element, idx) => element - array[idx]);
+  }
